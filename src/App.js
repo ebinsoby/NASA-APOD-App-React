b1bfe73770d82e5ded9f5e486cbf78c9a3929722
Ebin Soby <mailebinsoby@gmail.com>
2021-12-31 03:48:06 -0500

    route changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './Components/Home.js'
 
 function App() {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
     <div className='app'>
       <Route component={Home} path="/"  exact/>
       <Route  component={NasaPhoto} path="/nasaphoto"/>  
