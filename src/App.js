4d8a3c8144ae982dc85a8042c23952be2ef4de4a
Ebin Soby <mailebinsoby@gmail.com>
2021-12-31 01:57:00 -0500

    fixing routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import React from 'react'
-import {HashRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route} from 'react-router-dom'
 import NasaPhoto from './Components/NasaPhoto.js'
 import Home from './Components/Home.js'
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
     <div className='app'>
-      <Route component={Home} path="/"  exact/>
-      <Route  component={NasaPhoto} path="/nasaphoto"/>  
+      <Route component={Home} path="/nasa/"  exact/>
+      <Route  component={NasaPhoto} path="/nasa/nasaphoto"/>  
     </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
