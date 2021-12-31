528bd4bd50655abaff7f2a87ed4b03285f775f06
Ebin Soby <mailebinsoby@gmail.com>
2021-12-31 03:25:07 -0500

    using Hashrouter with basename

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './Components/Home.js'
 
 function App() {
   return (
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
     <div className='app'>
       <Route component={Home} path="/"  exact/>
       <Route  component={NasaPhoto} path="/nasaphoto"/>  
